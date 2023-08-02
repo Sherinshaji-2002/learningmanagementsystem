351b64f408d64327bcfe6e7ffb9410b0814bd8f3
Sherin Shaji <85390319+Sherinshaji-2002@users.noreply.github.com>
2023-08-02 22:34:28 +0530

    Add files via upload

---
+++ developers.php
@@ -0,0 +1,58 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Back</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="admin/images/sherin.jpg" class="img-circle">
+										<hr>
+										<p>Name: SHERIN SHAJI</p>
+										<p>Address: AEC</p>
+										<p>Email: sherinshaji@gmail.com</p>
+										<p>Position: Programmer</p>
+										</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/arathy.jpg" class="img-circle">
+								<hr>
+									    <p>Name: ARATHY CHANDRAN</p>
+
+										<p>Address: AEC</p>
+										<p>Email: arathychandran@gmail.com</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/devika.jpg" class="img-circle">
+								<hr>
+										<p>Name: DEVIKA B</p>
+										<p>Address: AEC</p>
+										<p>Email: devia@gmail.com</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                                
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
